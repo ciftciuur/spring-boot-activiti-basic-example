aeb3ac04038779f181f7bd0b31e667d7a714e9f3
Uğur Çiftçi <ugurciftci@beyazweb.com.tr>
2021-03-02 14:03:11 +0300

    added h2 db config and rename psql db connection

--- process/src/main/java/com/example/process/ProcessApplication.java
+++ process/src/main/java/com/example/process/ProcessApplication.java
@@ -28,68 +28,70 @@ import java.io.IOException;
 @EnableAutoConfiguration
 public class ProcessApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(ProcessApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(ProcessApplication.class, args);
+	}
 
-    /*
-        first project created processEngineConfiguration function changed -
-        config.setDatabaseSchemaUpdate("true"); ->
-        created db schema and default changed
-         config.setDatabaseSchemaUpdate("none");
-     */
-    @Bean
-    public SpringProcessEngineConfiguration processEngineConfiguration() {
-        SpringProcessEngineConfiguration config = new SpringProcessEngineConfiguration();
-        config.setDataSource(databaseConnection());
-        config.setTransactionManager(transactionManager());
-        config.setDatabaseSchemaUpdate("none");
-        config.setHistory("audit");
-        config.setAsyncExecutorActivate(true);
-        return config;
-    }
+	/*
+	 * first project created processEngineConfiguration function changed -
+	 * config.setDatabaseSchemaUpdate("true"); -> created db schema and default
+	 * changed config.setDatabaseSchemaUpdate("none");
+	 */
+	@Bean
+	public SpringProcessEngineConfiguration processEngineConfiguration() {
+		SpringProcessEngineConfiguration config = new SpringProcessEngineConfiguration();
+		config.setDataSource(h2DatabaseConnection());
+		config.setTransactionManager(transactionManager());
+		config.setDatabaseSchemaUpdate("none");
+		config.setHistory("audit");
+		config.setAsyncExecutorActivate(true);
+		return config;
+	}
 
+	@Bean
+	public PlatformTransactionManager transactionManager() {
+		return new DataSourceTransactionManager(h2DatabaseConnection());
+	}
 
-    @Bean
-    public PlatformTransactionManager transactionManager() {
-        return new DataSourceTransactionManager(databaseConnection());
-    }
+	@Bean
+	public ProcessEngineFactoryBean processEngine() {
+		ProcessEngineFactoryBean factoryBean = new ProcessEngineFactoryBean();
+		factoryBean.setProcessEngineConfiguration(processEngineConfiguration());
+		return factoryBean;
+	}
 
-    @Bean
-    public ProcessEngineFactoryBean processEngine() {
-        ProcessEngineFactoryBean factoryBean = new ProcessEngineFactoryBean();
-        factoryBean.setProcessEngineConfiguration(processEngineConfiguration());
-        return factoryBean;
-    }
+	@Bean
+	public DataSource postgreDatabaseConnection() {
+		return DataSourceBuilder.create().url("jdbc:postgresql://localhost:5432/process-db").username("postgres").password("postgres").driverClassName("org.postgresql.Driver").build();
+	}
 
-    @Bean
-    public DataSource databaseConnection() {
-        return DataSourceBuilder.create().url("jdbc:postgresql://localhost:5432/process-db").username("postgres")
-                .password("postgres").driverClassName("org.postgresql.Driver").build();
-    }
+	@Bean
+	public DataSource h2DatabaseConnection() {
+		return DataSourceBuilder.create().url("").username("").password("").driverClassName("").build();
+	}
 
-    @Bean
-    public RepositoryService repositoryService(ProcessEngine processEngine) {
-        return processEngine.getRepositoryService();
-    }
+	@Bean
+	public RepositoryService repositoryService(ProcessEngine processEngine) {
+		return processEngine.getRepositoryService();
+	}
 
-    @Bean
-    public RuntimeService runtimeService(ProcessEngine processEngine) {
-        return processEngine.getRuntimeService();
-    }
+	@Bean
+	public RuntimeService runtimeService(ProcessEngine processEngine) {
+		return processEngine.getRuntimeService();
+	}
 
-    @Bean
-    public TaskService taskService(ProcessEngine processEngine) {
-        return processEngine.getTaskService();
-    }
+	@Bean
+	public TaskService taskService(ProcessEngine processEngine) {
+		return processEngine.getTaskService();
+	}
 
-    @Bean
-    public IdentityService identityService(ProcessEngine processEngine) {
-        return processEngine.getIdentityService();
-    }
+	@Bean
+	public IdentityService identityService(ProcessEngine processEngine) {
+		return processEngine.getIdentityService();
+	}
 
-    @Bean
-    public ManagementService managementService(ProcessEngine processEngine) {
-        return processEngine.getManagementService();
-    }
+	@Bean
+	public ManagementService managementService(ProcessEngine processEngine) {
+		return processEngine.getManagementService();
+	}
 }
